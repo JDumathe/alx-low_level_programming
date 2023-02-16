e70dbaf745258a8e5e79061c81c682f4e45bcf15
JDumathe <mlu.m.mathe@gmail.com>
2023-02-16 07:07:04 -0800

    First update on low level programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - is the main function
+ * puts - writes string to stdout and a new line
+ * Return : is always zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
